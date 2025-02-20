ece5f8ef6c3d63cadbed80814fe9d5eb2103c36a
danil2205 <04danil@gmail.com>
2025-02-20 21:02:17 +0200

    feat: implement FileDatasource and MQTT publishing

--- agent/src/file_datasource.py
+++ agent/src/file_datasource.py
@@ -12,19 +12,61 @@ class FileDatasource:
         accelerometer_filename: str,
         gps_filename: str,
     ) -> None:
-        pass
-
+        self.accelerometer_filename = accelerometer_filename
+        self.gps_filename = gps_filename
+        self.acc_file = None
+        self.gps_file = None
+        self.acc_reader = None
+        self.gps_reader = None
+        
     def read(self) -> AggregatedData:
         """Метод повертає дані отримані з датчиків"""
+        try:
+            acc_data = next(self.acc_reader)
+        except StopIteration:
+            self.acc_file.seek(0)
+            next(self.acc_reader)
+            acc_data = next(self.acc_reader)
+
+        try:
+            gps_data = next(self.gps_reader)
+        except StopIteration:
+            self.gps_file.seek(0)
+            next(self.gps_reader)
+            gps_data = next(self.gps_reader)
+        
+        acc = Accelerometer(
+            int(acc_data[0]), 
+            int(acc_data[1]), 
+            int(acc_data[2])
+        )
+        
+        gps = Gps(
+            float(gps_data[0]),
+            float(gps_data[1])
+        )
+
         return AggregatedData(
-            Accelerometer(1, 2, 3),
-            Gps(4, 5),
+            acc,
+            gps,
             datetime.now(),
-            config.USER_ID,
+            config.USER_ID
         )
 
     def startReading(self, *args, **kwargs):
         """Метод повинен викликатись перед початком читання даних"""
+        self.acc_file = open(self.accelerometer_filename)
+        self.gps_file = open(self.gps_filename)
+        
+        self.acc_reader = reader(self.acc_file)
+        self.gps_reader = reader(self.gps_file)
+        
+        next(self.acc_reader)
+        next(self.gps_reader)
 
     def stopReading(self, *args, **kwargs):
         """Метод повинен викликатись для закінчення читання даних"""
+        if self.acc_file:
+            self.acc_file.close()
+        if self.gps_file:
+            self.gps_file.close()
